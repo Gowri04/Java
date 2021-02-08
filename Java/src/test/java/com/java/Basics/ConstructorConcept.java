e9db350fc5c27b87610fd88c27c7a94d6693e248
Gowri <Gowri@DESKTOP-AF33508.mshome.net>
2021-02-08 13:37:29 +0530

    commit

--- Java/src/test/java/com/java/Basics/ConstructorConcept.java
+++ Java/src/test/java/com/java/Basics/ConstructorConcept.java
@@ -20,92 +20,104 @@ public class ConstructorConcept
 	}
 }
 /*
-1.Constructor is special member in a class.
-
-2.Constructor have the same name as the classname.
-
-3.Constructor is used to create an object.
-
-4.Constructor can be used to initialize non static data members.
-
-5.Constructor will not return any value(not even void).
-
-6.Constructor will be invoked at the time of object creation
-
-7.Inside Constructor we can initialize variables.
-
-Type of Constructor
-
-a.Default Constructor
-
-1.Default constructors are created by compiler.
-2.Default constructors doesn't accept any arguments.
-3.Default constructors cannot initialize non static members it provides default values.
-
-b.Zero-Arg Constructor
-
-Zero Arg Constructor is created by programmers.
-We can initialize non static members in the body of the constructor.
-Zero-Arg Constructor doesn't accept any arguments.
-	
-c.Parameterized Constructor
-
-Parameterized Constructor is created by Programmer.
-In Parameterized Constructor we can initialize non static members with the help of arguments.
-In Parameterized Constructor we can pass values in the form of arguments.
 
 Constructor
+------------
 
 	1. Constructor is a block of codes similar to the method. 
-	2. Constructor is a special type of method which is used to initialize the object(create an object)
+	
+	2. Constructor is a special type of method which is used to initialize the object(create an object).
+	
 	3. Constructor is called when an instance of the class is created.
+	
 	4. Every time an object is created using the new() keyword, at least one constructor is called.
+	
 	5. If there is no constructor in a class, compiler automatically creates a default constructor.
 	
-Rules for creating Java constructor
+	6. Inside Constructor we can initialize variables.
+	
+	7. Constructor can be used to initialize non static data members.
+	
+	
+Rules for creating Java Constructor
+------------------------------------
 
-	1. Constructor name must be the same as its class name
-	2. Constructor doesn't have return type
-	3. Constructor cannot be abstract, static, final, and synchronized
+	1. Constructor name must be the same as its class name.
+	
+	2. Constructor doesn't have return type(not even void).
+	
+	3. Constructor cannot be abstract, static, final, and synchronized.
+	
 
 Note
+-----
 
-	1. We can use access modifiers while declaring a constructor. 
+	1. We can use access modifiers(private, protected, public or default constructor) while declaring a constructor. 
+	
 	2. It controls the object creation. 
-	3. We can have private, protected, public or default constructor in Java.
+	
 
 Types of Constructor
+------------------------
 
 	1. Default Constructor 
+	---------------------------
+	
 		a. Default Constructor is created by compiler.
-		b. We cannot initialize non static members with the default constructor as it provides default values(0,null)
+		
+		b. We cannot initialize non static members with the default constructor as it provides default values(0,null).
+		
 		c. Default Constructor doesn't accept any arguments.
+		
 	2. Zero Argument Constructor
+	-----------------------------
+	
 		a. Zero Argument Constructor is created by the programmer.
-		b. We can initialize non static members in the body of the constructor
-		c. Zero Argument Constructor doesn�t accept any arguments
+		
+		b. We can initialize non static members in the body of the constructor.
+		
+		c. Zero Argument Constructor doesn�t accept any arguments.
+		
 	3. Parameterized Constructor
+	-----------------------------
+	
 		a. Parameterized Constructor is created by the programmer.
+		
 		b. We can initialize non static members with the help of arguments.
+		
 		c. Parameterized Constructor we can pass the values in the form the arguments.
-		Parameterized Constructor is used to provide different values to distinct objects. 
+		
+		d. Parameterized Constructor is used to provide different values to distinct objects. 
 
 
-Java Constructor/Java Method
-
-Java Constructor
-	1. Constructor is used to initialize the state of an object.	
-	2. Constructor doesn't not have a return type.	
-	3. Constructor is invoked implicitly(called by compiler)	
-	4. Compiler provides a default constructor if we don't have any constructor in a class.	
-	5. Constructor name must be same as the class name.	
+Java Method/Java Constructor
+-----------------------------
 
 Java Method
+------------
+
 	1. Method is used to expose the behavior of an object.
+	
 	2. Method must have a return type.
-	3. Method is invoked explicitly(called by object)
+	
+	3. Method is invoked explicitly(called by object).
+	
 	4. Method is not provided by the compiler in any case.
-	5. Method name may or may not be same as the class name
+	
+	5. Method name may or may not be same as the class name.
+
+Java Constructor
+----------------
+
+	1. Constructor is used to initialize the state of an object.	
+	
+	2. Constructor doesn't not have a return type.	
+	
+	3. Constructor is invoked implicitly(called by compiler).	
+	
+	4. Compiler provides a default constructor if we don't have any constructor in a class.	
+	
+	5. Constructor name must be same as the class name.	
 
 
 
--- Java/src/test/java/com/java/OOPS/Polymorphism/RunTimePolymorphism.java
+++ Java/src/test/java/com/java/OOPS/Polymorphism/RunTimePolymorphism.java
@@ -24,3 +24,53 @@ public class RunTimePolymorphism
 		mo.method();
 	}
 }
+/*
+
+
+Method Overriding 
+
+If a child class provides the specific implementation of the method that has been declared by one of its parent class, it is known as method overriding.
+
+Usage of Java Method Overriding
+
+1. Method overriding is used to provide the specific implementation of a method which is already provided by its superclass.
+2. Method overriding is used for runtime polymorphism
+
+Rules for Java Method Overriding
+1. The method must have the same name as in the parent class
+2. The method must have the same parameter as in the parent class.
+3. There must be an IS-A relationship (inheritance).
+
+
+Can we override static method?
+No, a static method cannot be overridden
+
+Why can we not override static method?
+1. It is because the static method is bound with class whereas instance method is bound with an object. 
+2. Static belongs to the class area, and an instance belongs to the heap area.
+
+Can we override java main method?
+No, because the main is a static method.
+
+Method Overloading/Method Overriding 
+
+Method Overloading
+
+1. Method overloading is used to increase the readability of the program.	
+2. Method overloading is performed within class.	
+3. In case of method overloading, parameter must be different.	
+4. Method overloading is the example of compile time polymorphism.	
+5. In java, method overloading can't be performed by changing return type of the method only. Return type can be same or different in method overloading. But you must have to change the parameter.	
+
+
+Method Overriding 
+
+1. Method overriding is used to provide the specific implementation of the method that is already provided by its super class.
+2. Method overriding occurs in two classes that have IS-A (inheritance) relationship.
+3. In case of method overriding, parameter must be same.
+4. Method overriding is the example of run time polymorphism.
+5. Return type must be same or covariant in method overriding.
+
+
+
+*/
