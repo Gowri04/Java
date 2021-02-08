0909e94800ecb57c5803aaaaef48bb5b28320d58
Gowri <Gowri@DESKTOP-AF33508.mshome.net>
2021-02-08 19:01:00 +0530

    commit

--- Java/src/test/java/com/java/OOPS/Polymorphism/ThisKeyword.java
+++ Java/src/test/java/com/java/OOPS/Polymorphism/ThisKeyword.java
@@ -28,31 +28,42 @@ public class ThisKeyword
 }
 /*
 
-this keyword is used to refer a class varaible.
+This keyword 
+---------------
+
+This keyword is a reference variable that refers to the current class variable or object.
+
+Why we use This Keyword?
+------------------------
+
 We use this keyword if there is any diffidence between class varaibles and method parameters.
 
-This keyword 
-
-This  keyword is a reference variable that refers to the current object.
-
 Usage of java this keyword
+-----------------------------
 
-	1. this can be used to refer current class instance variable.
-	The this keyword can be used to refer current class instance variable. If there is ambiguity between the instance variables and parameters, this keyword resolves the problem of ambiguity.
-	Understanding the problem without this keyword
+1. this can be used to refer current class instance variable.
+	
+The this keyword can be used to refer current class instance variable. If there is ambiguity between the instance variables and parameters, this keyword resolves the problem of ambiguity.
 
-Note:
-If local variables(formal arguments) and instance variables are different, there is no need to use this keyword�
-	2. this can be used to invoke current class method (implicitly)
-	You may invoke the method of the current class by using the this keyword. If you don't use the this keyword, compiler automatically adds this keyword while invoking the method.
-	3. this() can be used to invoke current class constructor.
-	The this() constructor call can be used to invoke the current class constructor. It is used to reuse the constructor. In other words, it is used for constructor chaining.
-	4. this can be passed as an argument in the method call.
-	5. this can be passed as argument in the constructor call.
-	6. this can be used to return the current class instance from the method.
+2. this can be used to invoke current class method (implicitly)
+
+You may invoke the method of the current class by using the this keyword. If you don't use the this keyword, compiler automatically adds this keyword while invoking the method.
+
+3. this() can be used to invoke current class constructor.
+
+The this() constructor call can be used to invoke the current class constructor. It is used to reuse the constructor. In other words, it is used for constructor chaining.
+
+4. this can be passed as an argument in the method call.
+
+5. this can be passed as argument in the constructor call.
+
+6. this can be used to return the current class instance from the method.
 
 
+Note
+----
 
+If local variables(formal arguments) and instance variables are different, there is no need to use this keyword.
 
 
 
